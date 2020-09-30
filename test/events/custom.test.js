1dc3097eac95d2e0f4c4ae3943834d480f9fe03d
Jake Champion <me@jakechampion.name>
2020-09-30 15:07:03 +0100

    Restore custom.test.js

--- test/events/custom.test.js
+++ test/events/custom.test.js
@@ -50,7 +50,7 @@ describe('event', function () {
 		proclaim.equal(sent_data.context.component_name, 'custom-o-tracking');
 	});
 
-	it('should listen to a dom event and generate a component_id', function (done) {
+	it('should listen to a dom event and generate a component_id', function () {
 
 		const event = new CustomEvent('oTracking.event', {
 			detail: {
@@ -62,36 +62,31 @@ describe('event', function () {
 		});
 
 		document.body.setAttribute('data-o-component', 'o-tracking');
-		document.body.addEventListener('oTracking.event', async function(e) {
+		document.body.addEventListener('oTracking.event', function(e) {
 
+			const callback = sinon.spy();
 
-			try {
-				const callback = sinon.spy();
+			trackEvent(e, callback);
+			proclaim.ok(callback.called, 'Callback not called.');
 
-				await trackEvent(e, callback);
-				proclaim.ok(callback.called, 'Callback not called.');
+			const sent_data = callback.getCall(0).thisValue;
 
-				const sent_data = callback.getCall(0).thisValue;
+			proclaim.deepEqual(Object.keys(sent_data), ["system","context","user","device","category","action"]);
 
-				proclaim.deepEqual(Object.keys(sent_data), ["system","context","user","device","category","action"]);
+			// Event
+			proclaim.equal(sent_data.category, "video");
+			proclaim.equal(sent_data.action, "play");
+			proclaim.equal(sent_data.context.key, 'id');
+			proclaim.equal(sent_data.context.value, 51234);
+			proclaim.equal(typeof sent_data.context.component_id, "number");
+			proclaim.equal(sent_data.context.component_name, "o-tracking");
 
-				// Event
-				proclaim.equal(sent_data.category, "video");
-				proclaim.equal(sent_data.action, "play");
-				proclaim.equal(sent_data.context.key, 'id');
-				proclaim.equal(sent_data.context.value, 51234);
-				proclaim.equal(typeof sent_data.context.component_id, "string");
-				proclaim.equal(sent_data.context.component_name, "o-tracking");
-				done();
-			} catch (error) {
-				done(error);
-			}
 		});
 
 		document.body.dispatchEvent(event);
 	});
 
-	it('should throw custom error message if the tracking event object contains circular references', async function () {
+	it('should throw custom error message if the tracking event object contains circular references', function () {
 
 		const callback = sinon.spy();
 
@@ -103,15 +98,15 @@ Here are the paths in the data which are circular:
 [
     ".context.context.circular"
 ]`;
+
 		const errorMessageOnIOS10 = `o-tracking does not support circular references in the analytics data.
 Please remove the circular references in the data.
 Here are the paths in the data which are circular:
 [
     "[0].item.context.context.circular"
 ]`;
-
 		try {
-			await trackEvent(
+			trackEvent(
 				new CustomEvent("oTracking.event", {
 					detail: {
 						category: "slideshow",
